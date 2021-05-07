3b5950fec3280d3448a534fde8b8bd0751178bb9
YusufZ <yusuf.zarifyanov@icl-services.com>
2021-05-07 09:53:02 +0300

    Add text sidebar

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -13,10 +13,10 @@ const MainComponent = () => {
             mode="inline"
             defaultSelectedKeys={["4"]}
           >
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
-            <Menu.Item key="4">nav 4</Menu.Item>
+            <Menu.Item key="1">Валюта</Menu.Item>
+            <Menu.Item key="2">Акции</Menu.Item>
+            <Menu.Item key="3">Облигации</Menu.Item>
+            <Menu.Item key="4">Фонды</Menu.Item>
           </Menu>
         </Sider>
         <Content className="content_block">
