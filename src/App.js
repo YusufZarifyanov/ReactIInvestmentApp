4703e66625046ba965c9a6a45d390aba7b463fe4
rasulakhmedkhanov <77333501+rasulakhmedkhanov@users.noreply.github.com>
2021-05-12 12:32:00 +0300

    refactor(router): add plug to the Switch, change order of root route.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Layout } from 'antd';
 import Header from './components/Header/Header';
 import Briefcase from "./components/Briefcase";
 import ContentWindow from "./components/Content"
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 const data_briefcase = ["Валюта", "Акции", "Облигации", "Фонды"]
 const data_showcase = ["Меню", "Магазин", "Что-то еще", "Подарки"];
@@ -17,15 +17,16 @@ function App() {
           <Route path="/briefcase">
             <Briefcase data={data_briefcase} />
           </Route>
-          <Route exact path="/">
-            <ContentWindow />
-          </Route>
           <Route exact path="/showcase">
             <Briefcase data={data_showcase} />
           </Route>
           <Route exact path="/about">
             <ContentWindow />
           </Route>
+          <Route exact path="/">
+            <ContentWindow />
+          </Route>
+          <Redirect to="/" />
         </Switch>
       </Layout>
     </BrowserRouter>
