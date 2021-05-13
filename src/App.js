995273d8f75ff19992dd546bb1a8f59c640c2cd5
YusufMagic <yusuf.yusuf130@yandex.ru>
2021-05-13 20:40:27 +0300

    refactor(App.js): change routes, add new routs
    --/briefcase/briefcase_route
    --/showcase/showcase_route

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
 import './App.css';
 import { Layout } from 'antd';
 import Header from './components/Header/Header';
-import Briefcase from "./components/Briefcase";
+import Briefcase from "./components/Briefcase/Briefcase";
+import Showcase from "./components/Showcase/Showcase";
 import ContentWindow from "./components/ContentWindow"
 import { Switch, Route, Redirect } from 'react-router-dom';
-
-const data_briefcase = ["Валюта", "Акции", "Облигации", "Фонды"]
-const data_showcase = ["Меню", "Магазин", "Что-то еще", "Подарки"];
 
 function App() {
   return (
     <Layout style={{ position: "relative", overflow: "hidden" }}>
       <Header />
       <Switch>
-        <Route path="/briefcase">
-
-          <Briefcase data={data_briefcase} />
-        </Route>
-        <Route exact path="/showcase">
-          <Briefcase
-            data={data_showcase}
-            content_data={"showcase"}
-            url_params={{ url_start: "showcase" }}
-          />
-        </Route>
-        <Route exact path="/about">
-          <ContentWindow data={"about"} />
-        </Route>
-        <Route exact path="/data">
-          <ContentWindow data={"someData"} />
-        </Route>
-        <Route exact path="/">
-          <ContentWindow content_data={"homepage"} />
-        </Route>
+        <Route path="/" exact component={ContentWindow} />
+        <Route path="/briefcase" exact component={Briefcase} />
+        <Route path="/briefcase/briefcase_route" exact component={Briefcase} />
+        <Route path="/showcase" exact component={Showcase} />
+        <Route path="/showcase/showcase_route" exact component={Showcase} />
+        <Route path="/about" exact component={ContentWindow} />
         <Redirect to="/" />
       </Switch>
     </Layout>
