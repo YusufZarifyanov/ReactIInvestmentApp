da879b2addb6f53aeca0e59b769290a44b48e271
rasulakhmedkhanov <77333501+rasulakhmedkhanov@users.noreply.github.com>
2021-05-14 13:08:36 +0300

    file: relocate Showcase component.

---
+++ src/containers/Showcase/Showcase.js
@@ -0,0 +1,33 @@
+import { Layout } from "antd";
+import SideBar from "../../components/SideBar/SideBar";
+import ContentWindow from "../../components/ContentWindow/ContentWindow";
+
+const sbMenuItems = [
+  {
+    name: "Топ Просмотров",
+    route: '/showcase_route'
+  },
+  {
+    name: "Актуальное",
+    route: '/showcase_route',
+  },
+  {
+    name: "Взлеты и Падения дня",
+    route: '/showcase_route'
+  },
+  {
+    name: "События",
+    route: '/showcase_route'
+  },
+];
+
+const Showcase = () => {
+  return (
+    <Layout>
+      <SideBar typeCase={"showcase"} menuItems={sbMenuItems} />
+      <ContentWindow data={"showcase"} className="content_block" />
+    </Layout>
+  );
+};
+
+export default Showcase;
