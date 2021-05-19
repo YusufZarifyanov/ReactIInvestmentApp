49b4b88fc636c2443c1160ba7b28ffb19a799297
Anis Almukhametov <Anis.Almukhametov@icl-services.com>
2021-05-19 09:35:06 +0300

    refactor(SideBar): remove unneeded hooks

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,20 +1,14 @@
 import { Layout, Menu } from "antd";
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const SideBar = ({ menuItems, active }) => {
-  const [activeMenuItem, setActiveMenuItem] = useState(menuItems.find(item => item.path === active)?.path)
-
-  useEffect(() => {
-    setActiveMenuItem(menuItems.find(item => item.path === active)?.path)
-  },[active])
 
   return (
     <Layout.Sider>
       <Menu
         className="sidebar_block"
         mode="inline"
-        selectedKeys={[`${activeMenuItem && activeMenuItem}`]}
+        selectedKeys={[`${active}`]}
       >
         {menuItems && menuItems.map(el => (
           <Menu.Item key={el.path} icon={el.icon}>
