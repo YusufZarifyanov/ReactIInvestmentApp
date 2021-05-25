d5c71adfab877ab5fdb1c439b27ff14fe32115a7
Anis Almukhametov <Anis.Almukhametov@icl-services.com>
2021-05-25 17:24:47 +0300

    feat(Showcase): Switch between components with Sidebar in Showcase container

--- src/containers/Showcase/Showcase.js
+++ src/containers/Showcase/Showcase.js
@@ -26,18 +26,64 @@ const subMenuItems = [
   },
 ];
 
+const Topviews = () => {
+  return (
+    <div>TopViews</div>
+  )
+}
+
+const Upsdowns = () => {
+  return (
+    <div>Upsdowns</div>
+  )
+}
+
+const Events = () => {
+  return (
+    <div>Events</div>
+  )
+}
+
+const NotFound = () => {
+  return (
+    <div>NotFound</div>
+  )
+}
+
+const components = {
+  topviews: Topviews,
+  upsdowns: Upsdowns,
+  events: Events,
+  default: NotFound
+}
+
 const Showcase = () => {
   const { showcaseSubmenuId } = useParams();
+
+  let CurrentComponent
+
+  switch (showcaseSubmenuId) {
+    case "topviews":
+      CurrentComponent = components.topviews
+      break;
+    case "upsdowns":
+      CurrentComponent = components.upsdowns
+      break;
+    case "events":
+      CurrentComponent = components.events
+      break;
+    default:
+      CurrentComponent = components.default
+      break;
+  }
 
   return (
     <Layout>
       <SideBar
         menuItems={subMenuItems}
         activeMenuItem={`/showcase/${showcaseSubmenuId}`}
-    />
-      <ContentWindow>
-        this is super showcase
-      </ContentWindow>
+      />
+      <CurrentComponent />
     </Layout>
   );
 };
