200679560292f46c76d0a170ea48a3f227778600
Yusuf <yusuf.yusuf130@yandex.ru>
2021-05-26 11:33:49 +0300

    create(BriefcaseSecurities.js)
    --add new component for securities

---
+++ src/components/BriefcaseSecurities/BriefcaseSecurities.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { Card, Avatar } from "antd";
+import { Chart } from "@antv/g2";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import "./BriefcaseSecurities.css";
+
+const { Meta } = Card;
+
+const BriefcaseSecurities = ({ location }) => {
+  const dataElem = location.dataItem;
+  return (
+    <div>
+      <div className="container">
+        <div className="container__card">
+          <Card
+            style={{ width: 300 }}
+            cover={<img alt="example" src={dataElem.src} />}
+            actions={[
+              <SettingOutlined key="setting" />,
+              <EditOutlined key="edit" />,
+              <EllipsisOutlined key="ellipsis" />,
+            ]}
+          >
+            <Meta
+              title={dataElem.name}
+              description={`${dataElem.count} шт. - ${dataElem.cost} ${dataElem.currency}`}
+            />
+          </Card>
+        </div>
+
+        <div className="container__graphic"></div>
+      </div>
+    </div>
+  );
+};
+
+export default BriefcaseSecurities;
