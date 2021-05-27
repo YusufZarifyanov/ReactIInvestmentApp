3c83592825bf3b0e101363c84b3ee25068c07570
YusufMagic <yusuf.yusuf130@yandex.ru>
2021-05-28 01:02:55 +0300

    refactor(BriefcaseSecuritites
    --Add new component without graphic(пока)

--- src/components/BriefcaseSecurities/BriefcaseSecurities.js
+++ src/components/BriefcaseSecurities/BriefcaseSecurities.js
@@ -1,35 +1,48 @@
 import React, { useState, useEffect } from "react";
-import { Card, Avatar } from "antd";
-// import { Chart } from "@antv/g2";
-import {
-  EditOutlined,
-  EllipsisOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
 import styles from "./BriefcaseSecurities.module.scss";
-
-const { Meta } = Card;
+import { Layout } from "antd";
+import SideBar from "../SideBar/SideBar";
+import { subMenuItems } from "../../data/index";
+// import SecuritiesGraphic from "../SecuritiesGraphic/SecuritiesGraphic.jsx";
 
 const BriefcaseSecurities = ({ location }) => {
   const dataElem = location.dataItem;
   return (
-    <div className={styles.container}>
-      <Card
-        className={styles.card}
-        cover={<img alt="example" src={dataElem.src} />}
-        actions={[
-          <SettingOutlined key="setting" />,
-          <EditOutlined key="edit" />,
-          <EllipsisOutlined key="ellipsis" />,
-        ]}
-      >
-        <Meta
-          title={dataElem.name}
-          description={`${dataElem.count} шт. - ${dataElem.cost} ${dataElem.currency}`}
-        />
-      </Card>
-      <div className={styles.graphic}></div>
-    </div>
+    <Layout>
+      <SideBar
+        menuItems={subMenuItems}
+        // activeMenuItem={`/briefcase/${briefcaseSubmenuId}`}
+      />
+      <Layout.Content>
+        <div className={styles.container}>
+          <div className={styles.info}>
+            <div className={styles.securitiesType}>
+              <p className={styles.name}>{dataElem.name}</p>
+              <p
+                className={styles.value}
+              >{`Доходность за полгода ${dataElem.value}`}</p>
+              <p className={styles.description}>{`Я свободный текст! Заполни меня`}</p>
+              <img
+                alt="example"
+                src={dataElem.src}
+                className={styles.img}
+              ></img>
+            </div>
+            <div className={styles.securitiesPrice}>
+              <p className={styles.date}>Цена акции 27 мая 2021г.</p>
+              <p className={styles.price}>
+                {`${dataElem.currency} ${dataElem.cost}`}
+              </p>
+              <button className={styles.btn}>Приобрести</button>
+            </div>
+          </div>
+
+          <div className={styles.graphic}>
+            {/* <SecuritiesGraphic /> */}
+          </div>
+        </div>
+      </Layout.Content>
+    </Layout>
   );
 };
 
