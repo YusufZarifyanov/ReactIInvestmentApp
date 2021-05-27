b8a7aad2c5a696c8e958b2ad76633b140a13a4a2
YusufMagic <yusuf.yusuf130@yandex.ru>
2021-05-28 01:04:05 +0300

    refactor(Briefcase)
    --append subMenuItems in new file

--- src/containers/Briefcase/Briefcase.js
+++ src/containers/Briefcase/Briefcase.js
@@ -3,47 +3,13 @@ import SideBar from "../../components/SideBar/SideBar";
 import BriefcaseOverview from "../../components/BriefcaseOverview/BriefcaseOverview";
 import BriefcaseItem from "../../components/BriefcaseItem/BriefcaseItem" 
 import { useParams } from "react-router";
+import {subMenuItems} from "../../data/index"
 import {
   data_bonds,
   data_currency,
   data_fonds,
   data_shares,
 } from "../../data/data_briefcase/index";
-import {
-  BankOutlined,
-  DollarCircleOutlined,
-  EyeOutlined,
-  FileTextOutlined,
-  PercentageOutlined,
-} from "@ant-design/icons";
-
-const subMenuItems = [
-  {
-    name: "Обзор",
-    path: "/briefcase/review",
-    icon: <EyeOutlined />,
-  },
-  {
-    name: "Валюта",
-    path: "/briefcase/currency",
-    icon: <DollarCircleOutlined />,
-  },
-  {
-    name: "Акции",
-    path: "/briefcase/shares",
-    icon: <PercentageOutlined />,
-  },
-  {
-    name: "Облигации",
-    path: "/briefcase/bonds",
-    icon: <FileTextOutlined />,
-  },
-  {
-    name: "Фонды",
-    path: "/briefcase/funds",
-    icon: <BankOutlined />,
-  },
-];
 
 const Briefcase = () => {
   const { briefcaseSubmenuId } = useParams();
@@ -64,7 +30,6 @@ const Briefcase = () => {
     default:
       console.log("Нет таких значений");
   }
-  console.log(data)
   return (
     <Layout>
       <SideBar
