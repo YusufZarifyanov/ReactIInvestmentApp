16e0ecc4f481772a24eeab4ff401ba9be4ec8abf
Anis Almukhametov <Anis.Almukhametov@icl-services.com>
2021-06-03 14:09:53 +0300

    refactor(TopViews, top_views): remove overcoding, add Overview and Securities to TopViews, add names to top_views

--- src/components/TopViews/TopViews.js
+++ src/components/TopViews/TopViews.js
@@ -1,54 +1,44 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import { List, Card, Tabs } from "antd";
+import React, { useState } from 'react';
+import { Tabs } from "antd";
 import { subMenuBriefcase } from "../../data/sub_menu"
+import Overview from '../Overview/Overview';
+import Securities from '../Securities/Securities';
+import './TopViews.scss'
 
 const TopViews = ({ data }) => {
-  const [active, setActive] = useState("/briefcase/review")
-
-  const allData = useMemo(() => [].concat.apply([], Object.values(data)), [data]);
-
-  const [activeData, setActiveData] = useState(allData)
-
-  const currentData = Object.keys(data).find(key => key === active.split('/')[2])
-
-  useEffect(() => {
-    if (!currentData) {
-      setActiveData(allData)
-    } else {
-      setActiveData(data[currentData])
-    }
-  }, [currentData, allData, data])
+  const [security, setSecurity] = useState("review")
 
   function changeActiveKey(activeKey) {
-    setActive(activeKey)
+    setSecurity(activeKey)
   }
 
   return (
     <Tabs
       onChange={changeActiveKey}
-      activeKey={active}
+      activeKey={security}
+      centered
+      type="card"
+      tabPosition="top"
     >
-      {subMenuBriefcase.map(item => (
+      {subMenuBriefcase.map(subMenuItem => (
         <Tabs.TabPane
           tab={
             <span>
-              {item.icon}
-              {item.name}
+              {subMenuItem.icon}
+              {subMenuItem.name}
             </span>
           }
-          key={item.path}>
-          <List
-            dataSource={activeData && activeData}
-            renderItem={(item) => (
-              <Card
-                hoverable
-                style={{ width: 200 }}
-                cover={<img alt={item.name} src={item.src} />}
-              >
-                <Card.Meta title={item.name} description={item.cost} />
-              </Card>
-            )}
-          />
+          key={subMenuItem.path.split('/')[2]}>
+          {
+            subMenuItem.path.split('/')[2] === "review" ?
+              (
+                <Overview data={data} type="showcase" />
+              ) : (
+                <Securities
+                  data={data[security]?.data}
+                  securityType={security} />
+              )
+          }
         </Tabs.TabPane>
       ))}
     </Tabs>
