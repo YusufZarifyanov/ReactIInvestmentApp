896b26092f98d5b1fb5568ffe81d39c7911a1328
YusufMagic <yusuf.yusuf130@yandex.ru>
2021-06-06 21:12:37 +0300

    ConnectAPI(BriefcaseItem.js)
    --connect to api and take chart data for graph

--- src/components/SecuritiesGraphic/SecuritiesGraphic.js
+++ src/components/SecuritiesGraphic/SecuritiesGraphic.js
@@ -1,35 +1,9 @@
 import React, { Component, useEffect, useState } from "react";
 import Plot from "react-plotly.js";
 
-const Graph = ({ graphFlag, xRange }) => {
-  console.log(xRange)
-  const [state, setState] = useState({ data: [] });
+const Graph = ({ graphFlag, xRange, chartData }) => {
+  console.log(chartData)
 
-  useEffect(() => {
-    const endpoint = "https://data.cityofnewyork.us/resource/rc75-m7u3.json";
-
-    fetch(endpoint)
-      .then((response) => response.json())
-      .then((data) => {
-        setState({ data: data });
-      });
-  }, []);
-
-  // Change data structure
-  const transformData = (data) => {
-    let plot_data = [];
-
-    let x = [];
-    let y = [];
-    data.map((each) => {
-      x.push(each.date_of_interest);
-      y.push(each.case_count);
-    });
-    plot_data["x"] = x;
-    plot_data["y"] = y;
-
-    return plot_data;
-  };
   return (
     <div>
       {!graphFlag ? (
@@ -38,8 +12,10 @@ const Graph = ({ graphFlag, xRange }) => {
             {
               type: "scatter",
               mode: "lines",
-              x: transformData(state.data)["x"],
-              y: transformData(state.data)["y"],
+              // x: transformData(state.data)["x"],
+              // y: transformData(state.data)["y"],
+              y: chartData.yAxes.close,
+              x: chartData.xAxes,
               marker: { color: "black" },
               displayModeBar: false,
             },
@@ -49,7 +25,12 @@ const Graph = ({ graphFlag, xRange }) => {
             responsive: true,
           }}
           layout={{
-            xaxis: {range: xRange}
+            xaxis: {
+              // autorange: true,
+              // domain: [0, 1],
+              range: xRange,
+              type: "date",
+            },
           }}
           style={{ width: "80%", margin: "0 auto", height: "400px" }}
         />
@@ -62,49 +43,21 @@ const Graph = ({ graphFlag, xRange }) => {
           style={{ width: "80%", margin: "0 auto", height: "400px" }}
           data={[
             {
-              x: transformData(state.data)["x"],
+              x: chartData.xAxes,
 
-              close: [
-                116.019997, 116.610001, 117.910004, 118.989998, 119.110001,
-                119.75, 119.25, 119.040001, 120, 119.989998, 119.779999, 120,
-                120.080002, 119.970001, 121.879997, 121.940002, 121.949997,
-                121.629997, 121.349998, 128.75, 128.529999, 129.080002,
-                130.289993, 131.529999, 132.039993, 132.419998, 132.119995,
-                133.289993, 135.020004, 135.509995,
-              ],
+              close: chartData.yAxes.close,
 
               decreasing: { line: { color: "#7F7F7F" } },
 
-              high: [
-                116.510002, 116.860001, 118.160004, 119.43, 119.379997, 119.93,
-                119.300003, 119.620003, 120.239998, 120.5, 120.089996,
-                120.449997, 120.809998, 120.099998, 122.099998, 122.440002,
-                122.349998, 121.629997, 121.389999, 130.490005, 129.389999,
-                129.190002, 130.5, 132.089996, 132.220001, 132.449997,
-                132.940002, 133.820007, 135.089996, 136.270004,
-              ],
+              high: chartData.yAxes.high,
 
               increasing: { line: { color: "#17BECF" } },
 
               line: { color: "rgba(31,119,180,1)" },
 
-              low: [
-                115.75, 115.809998, 116.470001, 117.940002, 118.300003,
-                118.599998, 118.209999, 118.809998, 118.220001, 119.709999,
-                119.370003, 119.730003, 119.769997, 119.5, 120.279999,
-                121.599998, 121.599998, 120.660004, 120.620003, 127.010002,
-                127.779999, 128.160004, 128.899994, 130.449997, 131.220001,
-                131.119995, 132.050003, 132.75, 133.25, 134.619995,
-              ],
+              low: chartData.yAxes.low,
 
-              open: [
-                115.849998, 115.919998, 116.779999, 117.949997, 118.769997,
-                118.739998, 118.900002, 119.110001, 118.339996, 120, 119.400002,
-                120.449997, 120, 119.550003, 120.419998, 121.669998, 122.139999,
-                120.93, 121.150002, 127.029999, 127.980003, 128.309998,
-                129.130005, 130.539993, 131.350006, 131.649994, 132.460007,
-                133.080002, 133.470001, 135.520004,
-              ],
+              open:chartData.yAxes.open,
 
               type: "candlestick",
               xaxis: "x",
--- src/containers/BriefcaseItem/BriefcaseItem.js
+++ src/containers/BriefcaseItem/BriefcaseItem.js
@@ -1,18 +1,47 @@
 import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { subMenuBriefcase, subMenuShowcase } from "../../data/sub_menu";
 import { securities } from "../../data/briefcase/securities";
+// import chartData from "../../data/briefcase/ChartData"
 
 import styles from "./BriefcaseItem.module.scss";
 
 import { Layout, Space, Button } from "antd";
 import SideBar from "../../components/SideBar/SideBar";
 import Graph from "../../components/SecuritiesGraphic/SecuritiesGraphic.js";
+import unirest from "unirest";
 
 const BriefcaseItem = () => {
   const { securityType, tiker } = useParams();
   const [graph, setGraph] = useState(false);
-  const [xRange, setXRange] = useState(["2017-01-04", "2017-02-15"]);
+  const [xRange, setXRange] = useState([
+    "2021-05-30 15:9:19",
+    "2021-06-06 15:9:19",
+  ]);
+  const [chartData, setChartData] = useState(undefined);
+
+  useEffect(() => {
+    const API_KEY = "VAUVU4KVB5DXM9ED";
+    const API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=AAL&outputsize=full&apikey=${API_KEY}`;
+
+    fetch(API_Call)
+      .then((res) => res.json())
+      .then((json) => {
+        json = json["Time Series (Daily)"]
+        console.log(json)
+        const yAxes = {open: [], high: [], low: [], close: [], adjusted: []};
+        const xAxes = Object.keys(json);
+        for (let item of Object.keys(json)) {
+          yAxes.open.push(json[item]["1. open"]);
+          yAxes.high.push(json[item]["2. high"]);
+          yAxes.low.push(json[item]["3. low"]);
+          yAxes.close.push(json[item]["4. close"]);
+          yAxes.adjusted.push(json[item]["5. adjusted close"]);
+        }
+        setChartData({ xAxes, yAxes });
+      });
+  }, []);
+
   let dataElem;
 
   let hasParam = Object.keys(securities).find((key) => key === securityType);
@@ -94,19 +123,21 @@ const BriefcaseItem = () => {
     },
   ];
 
+  console.log(chartData);
+
   return (
     <Layout>
-      {
-        securities[securityType].data.find(item => item.tiker === tiker)
-          ? <SideBar
-            menuItems={subMenuBriefcase}
-            activeMenuItem={`/briefcase/${securityType}`}
-          />
-          : <SideBar
-            menuItems={subMenuShowcase}
-            activeMenuItem="/showcase/topviews"
-          />
-      }
+      {securities[securityType].data.find((item) => item.tiker === tiker) ? (
+        <SideBar
+          menuItems={subMenuBriefcase}
+          activeMenuItem={`/briefcase/${securityType}`}
+        />
+      ) : (
+        <SideBar
+          menuItems={subMenuShowcase}
+          activeMenuItem="/showcase/topviews"
+        />
+      )}
       <Layout.Content>
         <div className={styles.container}>
           <div className={styles.cards}>
@@ -159,7 +190,9 @@ const BriefcaseItem = () => {
               ))}
             </Space>
           </div>
-          <Graph graphFlag={graph} xRange={xRange} />
+          {chartData && (
+            <Graph graphFlag={graph} xRange={xRange} chartData={chartData} />
+          )}
         </div>
       </Layout.Content>
     </Layout>
