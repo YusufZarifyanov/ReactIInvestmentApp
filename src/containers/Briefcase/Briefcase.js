dc6706e0334ebf9d2c5e8fb38ffc753c8def083c
Yusuf <yusuf.yusuf130@yandex.ru>
2021-06-15 18:11:09 +0300

    dont take this commit pls! sry(

--- src/containers/Briefcase/Briefcase.js
+++ src/containers/Briefcase/Briefcase.js
@@ -3,36 +3,34 @@ import SideBar from "../../components/SideBar/SideBar";
 import Overview from "../../components/Overview/Overview";
 import Securities from "../../components/Securities/Securities";
 import { useParams } from "react-router";
-
+import { useEffect, useState } from "react";
 import { subMenuBriefcase } from "../../data/sub_menu";
 import securities from "../../data/briefcase/securities";
 import { useRedirect } from "../../hooks/useRedirect";
 
-
-const components = {
-  review: {
-    component: Overview,
-    data: securities,
-  },
-  currency: {
-    component: Securities,
-    data: securities.currency.data,
-  },
-  shares: {
-    component: Securities,
-    data: securities.shares.data,
-  },
-  bonds: {
-    component: Securities,
-    data: securities.bonds.data,
-  },
-  funds: {
-    component: Securities,
-    data: securities.funds.data,
-  },
-};
-
 const Briefcase = () => {
+  const [components, setComponents] = useState({
+    review: {
+      component: Overview,
+      data: securities,
+    },
+    currency: {
+      component: Securities,
+      data: securities.currency.data,
+    },
+    shares: {
+      component: Securities,
+      data: securities.shares.data,
+    },
+    bonds: {
+      component: Securities,
+      data: securities.bonds.data,
+    },
+    funds: {
+      component: Securities,
+      data: securities.funds.data,
+    },
+  });
   const { briefcaseSubmenuId } = useParams();
 
   const briefcase = {
@@ -40,12 +38,43 @@ const Briefcase = () => {
     someData: [],
   };
 
+  useEffect(async () => {
+    const securitiesKeys = Object.keys(securities);
+    for (let securityKey of securitiesKeys) {
+      let tickerString = "";
+      for (let ticker of securities[securityKey].tickers) {
+        tickerString += `${ticker},`;
+      }
+
+      let dataSecurity = await fetch(
+        "https://yahoo-finance-low-latency.p.rapidapi.com/v6/finance/quote?symbols=" +
+          tickerString,
+        {
+          headers: {
+            "x-rapidapi-key":
+              "a70d0b9072msh5b07905beb24538p18761bjsn718f790b01c0",
+            "x-rapidapi-host": "yahoo-finance-low-latency.p.rapidapi.com",
+            useQueryString: true,
+          },
+        }
+      )
+        .then((res) => res.json())
+        .catch((err) => console.log(err));
+      dataSecurity = dataSecurity.quoteResponse.result;
+      setComponents({
+        securityKey: dataSecurity,
+      });
+    }
+  }, []);
+
   const Component = useRedirect(
     components,
     "/briefcase/review",
     briefcaseSubmenuId,
     "review"
   );
+
+  console.log(components);
 
   return (
     <Layout>
--- src/data/briefcase/securities.js
+++ src/data/briefcase/securities.js
@@ -1,5 +1,5 @@
 
-const getSymbolData = async (ticker, numberOfSecurities) => {
+exports.getSymbolData = async (ticker, numberOfSecurities) => {
   const tickerInfo = await fetch(
     `https://finnhub.io/api/v1/stock/profile2?symbol=${ticker}&token=c304pgqad3ifkigc82q0`
   )
@@ -17,74 +17,54 @@ const getSymbolData = async (ticker, numberOfSecurities) => {
 // const fundsTickers = ["JNJ", "MA", "BAC", "HD"];
 // const bondsTickers = ["XOM", "TTD", "PYPL", "ASML"];
 
-const currencyTikers = ["AAPL", "MSFT"];
-const sharesTickers = [];
-const fundsTickers = [];
-const bondsTickers = [];
 
-const currencyInfo = [],
-  sharesInfo = [],
-  bondsInfo = [],
-  fundsInfo = [];
+// const currencyInfo = [],
+//   sharesInfo = [],
+//   bondsInfo = [],
+//   fundsInfo = [];
 
-// const socket = new WebSocket('wss://ws.finnhub.io?token=c304pgqad3ifkigc82q0');
 
-// // Connection opened -> Subscribe
-// socket.addEventListener('open', function (event) {
-//     socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'AAPL'}))
-//     socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'}))
-//     socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'IC MARKETS:1'}))
-// });
+// const getTickerData = async () => {
+  // for (let currency of currencyTikers) {
+  //   currencyInfo.push(await getSymbolData(currency, 2));
+  // }
 
-// // Listen for messages
-// socket.addEventListener('message', function (event) {
-//     console.log('Message from server ', event.data);
-// });
+  // for (let shares of sharesTickers) {
+  //   sharesInfo.push(await getSymbolData(shares, 3));
+  // }
 
-// // Unsubscribe
-//  var unsubscribe = function(symbol) {
-//     socket.send(JSON.stringify({'type':'unsubscribe','symbol': symbol}))
-// }
+  // for (let bonds of bondsTickers) {
+  //   bondsInfo.push(await getSymbolData(bonds, 4));
+  // }
 
-const getTickerData = async () => {
-  for (let currency of currencyTikers) {
-    currencyInfo.push(await getSymbolData(currency, 2));
-  }
+  // for (let funds of fundsTickers) {
+  //   fundsInfo.push(await getSymbolData(funds, 5));
+  // }
 
-  for (let shares of sharesTickers) {
-    sharesInfo.push(await getSymbolData(shares, 3));
-  }
-
-  for (let bonds of bondsTickers) {
-    bondsInfo.push(await getSymbolData(bonds, 4));
-  }
-
-  for (let funds of fundsTickers) {
-    fundsInfo.push(await getSymbolData(funds, 5));
-  }
-
-  return;
-};
-
-getTickerData();
+//   return;
+// };
 
 module.exports = {
   currency: {
     name: "Валюта",
-    data: currencyInfo,
+    data: [],
+    tickers: ["AAPL", "MSFT"],
   },
   shares: {
     name: "Акции",
-    data: sharesInfo,
+    data: [],
+    tickers: ["JNJ"],
   },
 
   bonds: {
     name: "Облигации",
-    data: bondsInfo,
+    data: [],
+    tickers: ["BAC"],
   },
 
   funds: {
     name: "Фонды",
-    data: fundsInfo,
+    data: [],
+    tickers: ["MA"],
   },
 };
