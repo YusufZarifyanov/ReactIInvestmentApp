32718cac99b2ab553efb65cfc81f951819f0adbe
Anis Almukhametov <Anis.Almukhametov@icl-services.com>
2021-06-17 23:39:34 +0300

    feat(Showcase,UpsDowns): add data for UpsDowns from redux store in Showcase; refactor in UpsDowns

--- src/containers/Showcase/Showcase.js
+++ src/containers/Showcase/Showcase.js
@@ -1,30 +1,39 @@
 import { Layout } from "antd";
 import SideBar from "../../components/SideBar/SideBar";
 import { useParams } from "react-router";
-import { upsDowns } from "../../data/showcase/ups_downs";
+// import { upsDowns } from "../../data/showcase/ups_downs";
 import { topViews } from "../../data/showcase/top_views";
 import { subMenuShowcase } from "../../data/sub_menu";
 import TopViews from "../../components/TopViews/TopViews";
 import UpsDowns from "../../components/UpsDowns/UpsDowns";
 import Events from "../../components/Events/Events";
+// import { fakeResponseForEvents as dataEvents } from '../../data/showcase/fakeResponseEvents';
 import { useRedirect } from "../../hooks/useRedirect";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchNews } from '../../store/slices/showcase/events';
 import { useEffect } from "react";
+import { fetchUpsDowns } from "../../store/slices/showcase/upsdowns";
+import * as selectors from "../../store/selectors/showcase/upsdowns";
 
 const Showcase = () => {
   const { showcaseSubmenuId } = useParams();
   const dispatch = useDispatch();
 
   useEffect(() => {
+    showcaseSubmenuId === "upsdowns" && dispatch(fetchUpsDowns("AAPL,MSFT,BABA,IBM,TSLA,INTC,BA"));
     showcaseSubmenuId === "events" && dispatch(fetchNews());
   }, []);
 
   const news = useSelector(state => state.showcase.events.news);
 
+  const upsdownsData = useSelector(selectors.upsdownsData);
+  const ups = useSelector(selectors.ups);
+  const downs = useSelector(selectors.downs);
+
   useEffect(() => {
+    showcaseSubmenuId === "upsdowns" && !upsdownsData && dispatch(fetchUpsDowns("AAPL,MSFT,BABA,IBM,TSLA,INTC,BA"));
     showcaseSubmenuId === "events" && !news && dispatch(fetchNews());
-  }, [showcaseSubmenuId, news, dispatch]);
+  }, [showcaseSubmenuId, upsdownsData, news, dispatch]);
 
   const components = {
     topviews: {
@@ -33,7 +42,18 @@ const Showcase = () => {
     },
     upsdowns: {
       component: UpsDowns,
-      data: upsDowns,
+      data: {
+        ups: {
+          name: "Взлеты",
+          data: ups,
+          // data: upsDowns.ups.data
+        },
+        downs: {
+          name: "Падения",
+          data: downs,
+          // data: upsDowns.downs.data
+        }
+      },
     },
     events: {
       component: Events,
