d5ddadc6fac6bbd49fa6e9457a12571923f1ffc6
Anis Almukhametov <Anis.Almukhametov@icl-services.com>
2021-06-22 12:30:13 +0300

    file(securityItem.js): delete unnecessary file

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import securities from './slices/securities';
 import events from './slices/events';
-import securityItem from './slices/securityItem';
 
 export const store = configureStore({
   reducer: {
     securities,
     events,
-    securityItem,
   }
 });
