3e3327975f7ac7c9ad1eabaf935249e858e6cb42
Yusuf <yusuf.yusuf130@yandex.ru>
2021-06-24 11:56:17 +0300

    refactor(BriefcaseOverview
    --add redux logic in Briefcase

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -3,17 +3,10 @@ import { Layout, List, Spin } from "antd";
 import { Link, useLocation } from "react-router-dom";
 import { getPathPartByOrdinalNumber } from "../../functions/getPathPartByOrdinalNumber";
 
-const Overview = ({ data, briefcaseCalculation, loading }) => {
+const Overview = ({ data, briefcaseCalculation }) => {
+  console.log(data);
   const { pathname } = useLocation();
-
-  const dataWithHeaders = {
-    currency: data[0],
-    shares: data[1],
-    bonds: data[2],
-    funds: data[3],
-  };
-
-  const securities = Object.keys(dataWithHeaders);
+  const dataKeys = Object.keys(data);
 
   return (
     <Layout.Content>
@@ -22,61 +15,61 @@ const Overview = ({ data, briefcaseCalculation, loading }) => {
         <Spin size="large"/>
       </div>
       ) : ( */}
-        <div className={styles.main}>
-          {briefcaseCalculation && (
-            <div className={styles.headerTotalSum}>
-              <h1>Общая сумма: {briefcaseCalculation.amount} $</h1>
-            </div>
-          )}
-          <div className={styles.body}>
-            {securities.map((security) => (
-              <div key={security} className={styles.card}>
-                <div className={styles.elemHeader}>{security}</div>
-                <div className={styles.elem}>
-                  <List
-                    dataSource={dataWithHeaders[security]}
-                    renderItem={(item) => (
-                      <Link
-                        to={{
-                          pathname: `/${getPathPartByOrdinalNumber(
-                            pathname,
-                            1
-                          )}/${getPathPartByOrdinalNumber(pathname, 2)}/${
-                            item.symbol
-                          }`,
-                          dataItem: item,
-                        }}
-                      >
-                        <List.Item className={styles.listItem}>
-                          <List.Item.Meta
-                            className={styles.meta}
-                            avatar={
-                              <img
-                                className={styles.img}
-                                src={`https://s3.polygon.io/logos/${item.symbol.toLowerCase()}/logo.png`}
-                                alt={item.symbol}
-                              ></img>
-                            }
-                            title={item.symbol}
-                            description={
-                              briefcaseCalculation
-                                ? `${2} шт. - ${item.regularMarketPrice} $`
-                                : `${item.regularMarketPrice} $`
-                            }
-                          />
-                          {briefcaseCalculation && (
-                            <div>{`${2 * item.regularMarketPrice} $`}</div>
-                          )}
-                        </List.Item>
-                      </Link>
-                    )}
-                  />
-                </div>
-                <div className={styles.makeColumnTall}></div>
-              </div>
-            ))}
+      <div className={styles.main}>
+        {briefcaseCalculation && (
+          <div className={styles.headerTotalSum}>
+            <h1>Общая сумма: {briefcaseCalculation.amount} $</h1>
           </div>
+        )}
+        <div className={styles.body}>
+          {dataKeys.map((securityKey) => (
+            <div key={securityKey} className={styles.card}>
+              <div className={styles.elemHeader}>{data[securityKey].name}</div>
+              <div className={styles.elem}>
+                <List
+                  dataSource={data[securityKey].data}
+                  renderItem={(item) => (
+                    <Link
+                      to={{
+                        pathname: `/${getPathPartByOrdinalNumber(
+                          pathname,
+                          1
+                        )}/${getPathPartByOrdinalNumber(pathname, 2)}/${
+                          item.symbol
+                        }`,
+                        dataItem: item,
+                      }}
+                    >
+                      <List.Item className={styles.listItem}>
+                        <List.Item.Meta
+                          className={styles.meta}
+                          avatar={
+                            <img
+                              className={styles.img}
+                              src={`https://s3.polygon.io/logos/${item.symbol.toLowerCase()}/logo.png`}
+                              alt={item.symbol}
+                            ></img>
+                          }
+                          title={item.symbol}
+                          description={
+                            briefcaseCalculation
+                              ? `${2} шт. - ${item.regularMarketPrice} $`
+                              : `${item.regularMarketPrice} $`
+                          }
+                        />
+                        {briefcaseCalculation && (
+                          <div>{`${2 * item.regularMarketPrice} $`}</div>
+                        )}
+                      </List.Item>
+                    </Link>
+                  )}
+                />
+              </div>
+              <div className={styles.makeColumnTall}></div>
+            </div>
+          ))}
         </div>
+      </div>
       {/* )} */}
     </Layout.Content>
   );
--- src/containers/Briefcase/Briefcase.js
+++ src/containers/Briefcase/Briefcase.js
@@ -5,14 +5,17 @@ import Securities from "../../components/Securities/Securities";
 import { useParams } from "react-router";
 import { useEffect, useState } from "react";
 import { subMenuBriefcase } from "../../data/sub_menu";
-import securities from "../../data/briefcase/securities";
 import { useRedirect } from "../../hooks/useRedirect";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchOverview } from "../../store/slices/securities";
+import { fetchSecurities } from "../../store/slices/securities";
+
+import { changeCurrentSecurity } from "../../store/slices/securities";
 
 const Briefcase = () => {
+  const [securityState, setSecurityState] = useState({});
   const dispatch = useDispatch();
-  const [loading, setLoading] = useState(true);
+
+  // const [loading, setLoading] = useState(true);
   const { briefcaseSubmenuId } = useParams();
 
   const briefcase = {
@@ -20,47 +23,56 @@ const Briefcase = () => {
     someData: [],
   };
 
-  useEffect(async () => {
-      dispatch(fetchOverview("BABA"));
-  }, []);
-  const currency = useSelector(state => state.securities.overview);
-  const shares = useSelector(state => state.securities.overview);
-  const bonds = useSelector(state => state.securities.overview);
-  const funds = useSelector(state => state.securities.overview);
-  const review = [].concat([currency, shares, bonds, funds]);
-  const components = {
-    review: {
-      component: Overview,
-      data: review,
-    },
-    currency: {
-      component: Securities,
-      data: currency,
-    },
-    shares: {
-      component: Securities,
-      data: shares,
-    },
-    bonds: {
-      component: Securities,
-      data: bonds,
-    },
-    funds: {
-      component: Securities,
-      data: funds,
-    },
-  }
+  // let securities = {
+  //   currency: useSelector((state) => state.securities.myBriefcase.currency),
+  //   bonds: useSelector((state) => state.securities.myBriefcase.bonds),
+  //   shares: useSelector((state) => state.securities.myBriefcase.shares),
+  //   funds: useSelector((state) => state.securities.myBriefcase.funds),
+  // };
+
+  // useEffect(() => {
+  //   dispatch(changeCurrentSecurity(briefcaseSubmenuId));
+  //   briefcaseSubmenuId === "review" &&
+  //     dispatch(fetchSecurities(overviewTickers));
+  //   briefcaseSubmenuId === "currency" &&
+  //     dispatch(fetchSecurities(securities.currency.tickers));
+  //   briefcaseSubmenuId === "bonds" &&
+  //     dispatch(fetchSecurities(securities.bonds.tickers));
+  //   briefcaseSubmenuId === "shares" &&
+  //     dispatch(fetchSecurities(securities.shares.tickers));
+  //   briefcaseSubmenuId === "funds" &&
+  //     dispatch(fetchSecurities(securities.funds.tickers));
+  // }, []);
+
+  const currency = useSelector(
+    (state) => state.securities.myBriefcase.currency
+  );
+  const bonds = useSelector((state) => state.securities.myBriefcase.bonds);
+  const shares = useSelector((state) => state.securities.myBriefcase.shares);
+  const funds = useSelector((state) => state.securities.myBriefcase.funds);
+
+  const securities = { currency, bonds, shares, funds };
+
+  let overviewTickers = [].concat([
+    currency.tickers,
+    shares.tickers,
+    bonds.tickers,
+    funds.tickers,
+  ]);
 
   useEffect(() => {
-    dispatch(fetchOverview("BABA"));
-  }, [components, dispatch])
-
-  const Component = useRedirect(
-    components,
-    "/briefcase/review",
-    briefcaseSubmenuId,
-    "review"
-  );
+    dispatch(changeCurrentSecurity(briefcaseSubmenuId));
+    briefcaseSubmenuId === "review" &&
+      dispatch(fetchSecurities(overviewTickers));
+    briefcaseSubmenuId === "currency" &&
+      dispatch(fetchSecurities(securities.currency.tickers));
+    briefcaseSubmenuId === "bonds" &&
+      dispatch(fetchSecurities(securities.bonds.tickers));
+    briefcaseSubmenuId === "shares" &&
+      dispatch(fetchSecurities(securities.shares.tickers));
+    briefcaseSubmenuId === "funds" &&
+      dispatch(fetchSecurities(securities.funds.tickers));
+  }, [briefcaseSubmenuId]);
 
   return (
     <Layout>
@@ -69,9 +81,11 @@ const Briefcase = () => {
         activeMenuItem={`/briefcase/${briefcaseSubmenuId}`}
       />
       {briefcaseSubmenuId === "review" ? (
-        <Component briefcaseCalculation={briefcase} loading={loading} />
+        <Overview briefcaseCalculation={briefcase} data={securities}></Overview>
       ) : (
-        <Component />
+        // <Component briefcaseCalculation={briefcase} loading={loading} />
+        <Securities data={securities[briefcaseSubmenuId].data}></Securities>
+        // <Component />
       )}
     </Layout>
   );
