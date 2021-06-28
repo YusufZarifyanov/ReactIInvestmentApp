b687bef9e08046d4bfacc4e9b399c754e5db1129
Anis Almukhametov <Anis.Almukhametov@icl-services.com>
2021-06-28 12:42:07 +0300

    feat(slices/events.js): add axios

--- src/store/slices/events.js
+++ src/store/slices/events.js
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { fakeResponseForEvents as dataEvents } from '../../data/showcase/fakeResponseEvents';
 import { setWarning } from "./modals";
+import axios from 'axios';
 
 const initialState = {
   news: [],
@@ -9,30 +10,32 @@ const initialState = {
 
 export const fetchNews = createAsyncThunk('events/fetchNews', async (_, { dispatch }) => {
   try {
-    const response = await fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/news/v2/list?region=US&snippetCount=28", {
-      method: "POST",
+    const response = await axios({
+      method: 'POST',
+      url: "https://apidojo-yahoo-finance-v1.p.rapidapi.com/news/v2/list?region=US&snippetCount=28",
       headers: {
         "content-type": "text/plain",
         "x-rapidapi-key": "f1e65c7abemshcd54427cb794343p12836fjsnc73c0f5b4b4a",
         "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-      },
-      body: "",
+      }
     });
 
-    const result = await response.json();
-    if (result.message) {
-      dispatch(setWarning(result.message))
-      return result
-    } else {
-      return result.data.main.stream
-    }
+    return response.data.data.main.stream;
     // return new Promise(function (resolve, reject) {
     //   setTimeout(() => {
     //     resolve(dataEvents.data.main.stream);
     //   }, 1500);
     // });
   } catch (error) {
-    console.log("fetchNews error-->", error.message)
+    if (error.response) {
+      console.log("fetchNews error in response", error.response);
+      dispatch(setWarning(error.response.data.message));
+      return error.response.data;
+    } else {
+      console.log('fetchNews Error -->', error);
+      dispatch(setWarning(error.message));
+      return error;
+    }
   }
 });
 
@@ -43,7 +46,7 @@ const slice = createSlice({
   extraReducers: builder => {
     builder
       .addCase(fetchNews.pending, (state) => {
-        state.loading = true
+        state.loading = true;
       })
       .addCase(fetchNews.fulfilled, (state, { payload: news }) => {
         if (!news.message) {
